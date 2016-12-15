d398fae7a5dc1d26ff47743d2ca13a7574775549
yqt2016 <170503122@qq.com>
2016-12-15 15:55:25 +0800

    装饰器的两种写法

--- decorator.py
+++ decorator.py
@@ -1,5 +1,34 @@
 #!/usr/bin/env python
 #FileName:decorator.py
+import functools
 def now():
 	print '2016-12-15'
+print(now.__name__)
+
+def log(func):
+	@functools.wraps(func)
+	def wrapper(*args,**kw):
+		print 'begin call'
+		func(*args,**kw)
+		print 'end call'
+	return wrapper
+now=log(now)
 now()
+print(now.__name__)
+
+def now2():
+	print '2016-12-15 15:00'
+
+def log2(text):
+	def decorator(func):
+		@functools.wraps(func)
+		def wrapper(*args,**kw):
+			print 'begin %s'%text
+			func(*args,**kw)
+			print 'end %s'%text
+		return wrapper
+	return decorator
+now2=log2('call')(now2)
+now2()
+print(now2.__name__)
+		
